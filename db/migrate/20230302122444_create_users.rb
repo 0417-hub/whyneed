d114b339a8f08bf2e85ecd12d053378e2710f195
KazuyaMutoh <mfshm0417@gmail.com>
2023-03-02 21:28:40 +0900

    Add: userのmigrationをmigrate

---
+++ db/migrate/20230302122444_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :line_id, null: false
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end

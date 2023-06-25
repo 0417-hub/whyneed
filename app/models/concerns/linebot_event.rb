9e8e2b804447d0ba00c9e82911e3e479d6b7fcf6
KazuyaMutoh <mfshm0417@gmail.com>
2023-06-26 07:50:37 +0900

    Add: 条件分岐の追加

--- app/models/concerns/linebot_event.rb
+++ app/models/concerns/linebot_event.rb
@@ -42,7 +42,66 @@ module LinebotEvent
             message = {type: "text", text: "#{event.message["text"]}が送られました"}
           end
           client.reply_message(event['replyToken'], message)
+
+          # ここから1週間後の書いてく
+          case @user.status
+          when 'first_message' #別のものに変える
+            case replied_message
+            when '1'
+              #@response.add_cancel_message #やり取り中止したで
+              @response.add_bought_message #購入した場合のメッセージ
+            when '2'
+              #@response.add_asking_trash_name_message #ゴミの名前聞くメッセ送信
+              #@user.registration!
+              @response.add_stop_message #買わなかった場合のメッセージ
+            when '3'
+              #@response.add_show_trashes_message(@user) #ゴミリスト送信
+              @response.add_think_message #再検討の場合のメッセージ
+            else
+              @response.add_alert_message #正しく入力してやー
+            end
+
+            if 3 == message
+              #add_think_messageを送信
+              @response.add_think_message
+              item.status = "think"
+            elsif 1 == message
+              #add_bought_messageを送信
+              @response.add_bought_message
+              item.status = "buy"
+            elsif 2 == message
+              #add_stop_messageを送信
+              @response.add_stop_message
+              item.status = "stop"
+            end
+
+            if item.status == "buy" && item.updated_at < 1.month.ago
+              #item.statusがbuyで、updated_atが1ヶ月より前の場合の処理
+              @response.add_one_month_message
+              if message == 1
+                @response.add_like_message
+              elsif message == 2
+                @response.add_not_like_message
+              end
+            end
+
+            if item.status == "think" && item.updated_at < 1.week.ago
+              #item.statusがthinkで、updated_atが1週間より前の場合の処理
+              @response.add_first_message
+            elsif item.status == "think" && item.updated_at < 2.weeks.ago
+                  # updated_atが2週間前で、statusがthinkの場合の処理
+              @response.add_think_more_message
+            elsif item.status == "think" && item.updated_at < 3.weeks.ago
+                  # updated_atが3週間前で、statusがthinkの場合の処理
+              @response.add_think_more_two_message
+            elsif item.status == "think" && item.updated_at < 4.weeks.ago
+                  # updated_atが4週間前で、statusがthinkの場合の処理
+              @response.add_think_end_message
+            end
+            
+
+          end
         end
-      end
+    end
   end
 end

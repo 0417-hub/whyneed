a2692cfd6df58545e8f5d37a760e5d963447f13d
KazuyaMutoh <mfshm0417@gmail.com>
2023-08-08 23:35:15 +0900

    Add: enumでstatusの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,20 @@ class Item < ApplicationRecord
                                       #  where(status: [1, 3, 7, 11])
 
   # TODO: statusのenumを定義する！(リプライ待ちとそうでないitemをわかりやすく命名する)
-  enum status: { 1: 'waiting_for_first_message_reply', 2: 'not_waiting_received_url', 3: 'waiting_for_one_month_message_reply', 4: 'not_waiting_stop_the_buy_message', 5: 'waiting_for_second_weeks_message_reply', 6: 'not_waiting_within_one_month_message', 7: 'waiting_for_third_weeks_message_reply', 8: 'not_waiting_after_send_like_message', 9: 'waiting_for_fourth_weeks_message_reply', 10: 'not_waiting_after_send_not_like_message', 12: 'not_waiting_after_send_add_think_message', 14: 'not_waiting_after_send_add_think_more_message', 16: 'not_waiting_after_send_add_think_more_two_message', 18: 'not_waiting_after_send_add_think__end_message',}
+  enum status: { 
+    waiting_for_first_message_reply: 1, # ユーザーからの返信待ち
+    not_waiting_received_url: 2,
+    waiting_for_one_month_message_reply: 3, # ユーザーからの返信待ち
+    not_waiting_stop_the_buy_message: 4,
+    waiting_for_second_weeks_message_reply: 5,# ユーザーからの返信待ち
+    not_waiting_within_one_month_message: 6,
+    waiting_for_third_weeks_message_reply: 7, # ユーザーからの返信待ち
+    not_waiting_after_send_like_message: 8,
+    waiting_for_fourth_weeks_message_reply: 9, # やり取り終了
+    not_waiting_after_send_not_like_message: 10,
+    not_waiting_after_send_add_think_message: 12,
+    not_waiting_after_send_add_think_more_message: 14,
+    not_waiting_after_send_add_think_more_two_message: 16,
+    not_waiting_after_send_add_think__end_message: 18
+  }  
 end

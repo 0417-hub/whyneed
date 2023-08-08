aba51edc15122a702ee178c2db9f5c278a63992e
KazuyaMutoh <mfshm0417@gmail.com>
2023-08-08 23:36:36 +0900

    Add: wheneverの設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,28 @@ set :environment, Rails.env.to_sym
 # 実行logの出力先
 set :output, "#{Rails.root.to_s}/log/cron.log"
 
-every 1.week, at: '19:00pm' do # 1.minute 1.day 1.week 1.month 1.yearなどをサポート
+every 1.day, at: '19:00pm' do # 1.minute 1.day 1.week 1.month 1.yearなどをサポート
   rake "response:check_today"
 end
+
+every 1.day, at: '19:00pm' do # 1.minute 1.day 1.week 1.month 1.yearなどをサポート
+  rake "bought_tasks:send_buy_message"
+end
+
+every 1.day, at: '19:00pm' do # 1.minute 1.day 1.week 1.month 1.yearなどをサポート
+  rake "think_tasks:send_think_message"
+end
+
+every 1.day, at: '19:00pm' do # 1.minute 1.day 1.week 1.month 1.yearなどをサポート
+  rake "think_tasks:send_think_more_message"
+end
+
+every 1.day, at: '19:00pm' do # 1.minute 1.day 1.week 1.month 1.yearなどをサポート
+  rake "think_tasks:send_think_more_two_message"
+end
+
+every 1.day, at: '19:00pm' do # 1.minute 1.day 1.week 1.month 1.yearなどをサポート
+  rake "think_tasks:send_think_end_message"
+end
+
 
